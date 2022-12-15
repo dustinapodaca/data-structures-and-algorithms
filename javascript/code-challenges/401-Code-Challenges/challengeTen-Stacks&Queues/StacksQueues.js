c960898b132d6b78ef2d82306a0234e846149021
dustinapodaca <dustinapodaca@gmail.com>
2022-12-14 22:04:51 -0700

    added for challenge 13

--- javascript/code-challenges/401-Code-Challenges/challengeTen-Stacks&Queues/StacksQueues.js
+++ javascript/code-challenges/401-Code-Challenges/challengeTen-Stacks&Queues/StacksQueues.js
@@ -125,50 +125,78 @@ class AnimalShelter {
   }
 }
 
-const animalShelter = new AnimalShelter();
-animalShelter.enqueue({ type: 'dog', name: 'dog1' });
-animalShelter.enqueue({ type: 'dog', name: 'dog2' });
-animalShelter.enqueue({ type: 'dog', name: 'dog3' });
-animalShelter.enqueue({ type: 'cat', name: 'cat1' });
-animalShelter.enqueue({ type: 'cat', name: 'cat2' });
-animalShelter.enqueue({ type: 'cat', name: 'cat3' });
-console.log(animalShelter.dequeue('dog'));
-console.log(animalShelter.dequeue('cat'));
-console.log(animalShelter.dequeue('dog'));
-console.log(animalShelter.dequeue('cat'));
-console.log(animalShelter.dequeue('dog'));
-console.log(animalShelter.dequeue('cat'));
+const validateBrackets = (str) => {
+  const stack = new Stack();
+  const open = ['(', '[', '{'];
+  const close = [')', ']', '}'];
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) { // if open bracket
+      stack.push(str[i]); // push open brackets to stack
+      console.log(JSON.stringify(stack));
+    } else if (close.includes(str[i])) { // if close bracket
+      if (stack.isEmpty()) { // if stack is empty
+        return false; // return false
+      } else if (open.indexOf(stack.pop()) !== close.indexOf(str[i])) { // if open bracket does not match close bracket
+        return false; // return false
+      }
+    }
+  }
+  if (stack.isEmpty()) { // if stack is empty
+    console.log(JSON.stringify(stack));
+    return true; // return true
+  } else { // if stack is not empty
+    return false; // return false
+  }
+};
 
-const pseudoQueue = new PseudoQueue();
-pseudoQueue.enqueue(20);
-pseudoQueue.enqueue(15);
-pseudoQueue.enqueue(10);
-console.log(JSON.stringify(pseudoQueue));
-console.log(pseudoQueue.dequeue());
-console.log(pseudoQueue.dequeue());
-console.log(JSON.stringify(pseudoQueue));
-console.log(pseudoQueue.dequeue());
-pseudoQueue.enqueue(5);
-console.log(pseudoQueue.dequeue());
+console.log(validateBrackets('(){}[[]]'));
 
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.push(4);
-stack.push(5);
-console.log(stack.peek());
-console.log(stack.pop());
-console.log(stack.peek());
-console.log(stack.isEmpty());
 
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-queue.enqueue(4);
-queue.enqueue(5);
-console.log(queue.peek());
-console.log(queue.dequeue());
-console.log(queue.peek());
-console.log(queue.isEmpty());
+
+// const animalShelter = new AnimalShelter();
+// animalShelter.enqueue({ type: 'dog', name: 'dog1' });
+// animalShelter.enqueue({ type: 'dog', name: 'dog2' });
+// animalShelter.enqueue({ type: 'dog', name: 'dog3' });
+// animalShelter.enqueue({ type: 'cat', name: 'cat1' });
+// animalShelter.enqueue({ type: 'cat', name: 'cat2' });
+// animalShelter.enqueue({ type: 'cat', name: 'cat3' });
+// console.log(animalShelter.dequeue('dog'));
+// console.log(animalShelter.dequeue('cat'));
+// console.log(animalShelter.dequeue('dog'));
+// console.log(animalShelter.dequeue('cat'));
+// console.log(animalShelter.dequeue('dog'));
+// console.log(animalShelter.dequeue('cat'));
+
+// const pseudoQueue = new PseudoQueue();
+// pseudoQueue.enqueue(20);
+// pseudoQueue.enqueue(15);
+// pseudoQueue.enqueue(10);
+// console.log(JSON.stringify(pseudoQueue));
+// console.log(pseudoQueue.dequeue());
+// console.log(pseudoQueue.dequeue());
+// console.log(JSON.stringify(pseudoQueue));
+// console.log(pseudoQueue.dequeue());
+// pseudoQueue.enqueue(5);
+// console.log(pseudoQueue.dequeue());
+
+// const stack = new Stack();
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+// stack.push(4);
+// stack.push(5);
+// console.log(stack.peek());
+// console.log(stack.pop());
+// console.log(stack.peek());
+// console.log(stack.isEmpty());
+
+// const queue = new Queue();
+// queue.enqueue(1);
+// queue.enqueue(2);
+// queue.enqueue(3);
+// queue.enqueue(4);
+// queue.enqueue(5);
+// console.log(queue.peek());
+// console.log(queue.dequeue());
+// console.log(queue.peek());
+// console.log(queue.isEmpty());
